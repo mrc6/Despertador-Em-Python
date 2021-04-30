ba4a368a15ad240d4032a6b69e5f2faefd344778
Marco Barbosa <marco.barbosa@base2.com.br>
2021-04-30 11:25:42 -0300

    Despertador/Alarme em python

---
+++ Alarm-timer.bat
@@ -0,0 +1,3 @@
+set VIRTUAL_ENV=C:\Users\Marco Barbosa\Documents\Marco\python\primeiro_projeto\alarm-timer
+start "%VIRTUAL_ENV%\python.exe" "%VIRTUAL_ENV%\main.py"
+echo %VIRTUAL_ENV%\python.exe %VIRTUAL_ENV%\main.py
